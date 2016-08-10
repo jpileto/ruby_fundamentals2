fa3b4f0f86207b124de4ed257b089d0f6dd1cd13
jpileto <jp.ileto@icloud.com>
2016-08-10 15:49:14 -0400

    Doing exercise 6 number 4

--- exercise6.rb
+++ exercise6.rb
@@ -1,1 +1,29 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+
+
+
+
+#a.each {|x| print x, " -- " }
+
+#a.each do |x|
+#  print x, " -- "
+#end
+
+#grocery_list << "rice"
+
+grocery_list << "rice"
+grocery_list.each {|x| puts "* #{x}"}
+
+def list(g)
+  g = grocery_list.each {|x| puts "* #{x}"}
+end
+
+puts grocery_list.size
+
+grocery_list.include?("bananas")
+if false
+puts "You don't need to pick up bananas today"
+else
+  puts "You need to pick up bananas"
+end
